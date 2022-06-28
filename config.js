f4f59a172a9f71882ba42ace1cda41b1b16f7e7f
Trunki Enqark <89256879+TrunkiEnqark@users.noreply.github.com>
2022-06-28 13:47:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
     titleWeb: "Kiếp sau làm lại luôn",
     introTitle: 'Heluuuuuuu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Nhớ trả lời thật lòng nhaaaaaaaaa`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
